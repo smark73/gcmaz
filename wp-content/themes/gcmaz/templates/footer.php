6763d4a26c7db8ebbf11c5c9281fd46f91508b97
Stacy at home <stacy@stacymark.com>
2014-06-26 08:47:39 -0700

    initial commit

---
+++ wp-content/themes/gcmaz/templates/footer.php
@@ -0,0 +1,26 @@
+<footer class="pg-ftr content-info container row" role="contentinfo">
+    <section class="ftr1 col-xs-6 col-sm-6 col-md-3 col-lg-3">
+        <ul class="ftr-links-list centered">
+            <li><span class="glyphicon glyphicon-home"></span><a href="/">Home</a></li>
+            <li><span class="glyphicon glyphicon-earphone"></span><a href="contact-great-circle-media">Contact Us</a></li>
+        </ul>
+        <br/><br/>
+    </section>
+    <section class="ftr2 col-md-6 col-lg-6 hidden-sm hidden-xs">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+    </section>
+    <section class="ftr3 col-xs-6 col-sm-6 col-md-3 col-lg-3 text-center">
+        <a href="/"><img src="/media/logo-gcmaz.png" width="125" height="auto" alt="Great Circle Media Arizona"/></a>
+        <p>Great Circle Media</p>
+        <p>&copy; Great Circle Media. All Rights Reserved</p>
+        <p>
+            <a href="http://www.gcmaz.com/legal/GeneralContestRules.pdf" target="_blank" class="ftr-sm">Contest Rules</a>
+            &nbsp;|&nbsp;
+            <a href="/employment/" class="ftr-sm">Employment</a>
+            &nbsp;|&nbsp;
+            <a href="http://www.gcmaz.com/legal/eeo.pdf" target="_blank" class="ftr-sm">EEO</a>
+        </p>
+    </section>
+</footer>
+<div class="clear-fix"><br/></div>
+<?php wp_footer(); ?>
