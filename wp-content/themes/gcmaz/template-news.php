64f0e333af756f2fe24a740ec250a1acc5de4621
Stacy at home <stacy@stacymark.com>
2014-07-03 09:16:33 -0700

    updates to css update layout in template-news.php

--- wp-content/themes/gcmaz/template-news.php
+++ wp-content/themes/gcmaz/template-news.php
@@ -5,20 +5,20 @@ Template Name: News Template
 ?>
 
 <div class="in-cnt-wrp row">
-    <section class="boxy row">
-        <div class="rbn-hdg">
-            <span class="centered txtshdw gen-hdr">Northern Arizona News</span>
-        </div>
-        <?php $query6 = new WP_Query(array(
+    <div class="rbn-hdg">
+        <span class="centered txtshdw gen-hdr">Northern Arizona News</span>
+    </div>
+    <section class="row">
+        <?php $query = new WP_Query(array(
             'category_name' => 'news',
             'posts_per_archive_page' => '3',
             'order' => 'DESC',
             ));
         ?>
-        <?php if($query6->have_posts()) : ?>
+        <?php if($query->have_posts()) : ?>
         
-            <?php while($query6->have_posts()) : $query6->the_post(); ?>
-                <section class="archv-pg-lstng row">
+            <?php while($query->have_posts()) : $query->the_post(); ?>
+                <section>
                     <?php
                         if(has_post_thumbnail()){
                             the_post_thumbnail('thumbnail pull-left');
@@ -31,7 +31,7 @@ Template Name: News Template
                 </section>
             <?php endwhile;?>
         
-            <?php if ($query6->max_num_pages > 1) : ?>
+            <?php if ($query->max_num_pages > 1) : ?>
               <nav class="post-nav">
                 <ul class="pager">
                   <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
@@ -50,14 +50,5 @@ Template Name: News Template
                 <?php _e('Sorry, no results were found.', 'roots'); ?>
             </div>
         <?php endif;?>
-    </section>
-        
-    <section class="indx-bnr-wrap row ">
-        <article class="indx-bnr col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <?php echo adrotate_group(4); ?>
-        </article>
-        <article class="indx-bnr col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <?php echo adrotate_group(5); ?>
-        </article>
     </section>
 </div>
