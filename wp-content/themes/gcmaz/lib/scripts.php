afc05c0143b5bd57dbe2a76e39352810133e8990
Stacy at home <stacy@stacymark.com>
2014-07-23 15:26:09 -0700

    update featured image class on splash post

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e25b6ed382f01252de96ed09653165db');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'de8331aa1845f4d9b016ea5dc7b0a355');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
--- wp-content/themes/gcmaz/single-splash-post.php
+++ wp-content/themes/gcmaz/single-splash-post.php
@@ -4,6 +4,6 @@ Template Name: Splash Pages
 */
 ?>
 <div class="centered row">
-<?php if(has_post_thumbnail()){ the_post_thumbnail('full', array('class'=>'centered info-img img-responsive'));} ?>
+<?php if(has_post_thumbnail()){ the_post_thumbnail('full', array('class'=>'centered splash-img img-responsive'));} ?>
 <?php get_template_part('templates/content', 'page'); ?>
 </div>
