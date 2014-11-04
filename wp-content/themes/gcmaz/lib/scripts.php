ddbcf1342cfa860faec1fac87818199804b7bbf4
Stacy at home <stacy@stacymark.com>
2014-11-04 10:02:26 -0800

    update listen button function - rmv jquery - added back to page content

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -28,7 +28,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '75bbf136e5b8aaa4d9353c06af3ba68e', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'cacccbb79dc5ca0da673f60882dcf07e', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
