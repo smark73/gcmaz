a686e44df6307a4b7647ca21abfc62a5e4ced43b
Stacy at home <stacy@stacymark.com>
2015-01-12 10:04:16 -0800

    update featured news images max height updated kaff news link in footer

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6b5134e1338ea61b945e9101d7e3cf56');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd26a1be179f320dfa403294e8843dfa4');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
