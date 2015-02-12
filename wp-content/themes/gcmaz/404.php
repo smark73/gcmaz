3de1488dcb28df6c34ddf9364add1397732fcafd
Stacy at home <stacy@stacymark.com>
2015-02-11 16:17:17 -0800

    fix up 404 page

--- wp-content/themes/gcmaz/404.php
+++ wp-content/themes/gcmaz/404.php
@@ -1,17 +1,27 @@
-<?php get_template_part('templates/page', 'header'); ?>
-
+<br/><br/>
 <div class="row">
     <div class="col-md-4">
-        <?php get_search_form(); ?>
     </div>
     <div class="col-md-8">
+        <h4 class="blue">
+            Oops.  The page you were looking for isn't here.
+        </h4>
+        
         <div class="alert alert-warning">
-          <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+          <?php _e('404 - Page Not Found', 'roots'); ?>
         </div>
-        <p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
+        
+        <p>Please search again:</p>
+        <?php get_search_form(); ?>
+
+        <br/><br/>
+        <p class="red"><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
         <ul>
           <li><?php _e('a mistyped address', 'roots'); ?></li>
           <li><?php _e('an out-of-date link', 'roots'); ?></li>
         </ul>
+        
     </div>
+    <br/><br/>
 </div>
+<br/><br/>
