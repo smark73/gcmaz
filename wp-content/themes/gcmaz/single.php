a801c273e3753031c7448913ef0c26f379a978fb
Stacy at home <stacy@stacymark.com>
2015-02-13 12:49:30 -0800

    update single post layout to show tags

--- wp-content/themes/gcmaz/single.php
+++ wp-content/themes/gcmaz/single.php
@@ -3,6 +3,14 @@
     <?php get_template_part('templates/entry-meta'); ?>
     <div class="clearfix"></div>
 </section>
+<section class="post-tags">
+    <?php
+        // if post has tags list them
+        if(get_the_tag_list()) {
+            echo get_the_tag_list('<ul><span>Tags: </span><li class="btn btn-default btn-xs">', '</li><li class="btn btn-default btn-xs"> ', '</li></ul>');
+        }
+    ?>
+</section>
 <section class="post-comments">
     <?php
         // If comments are open or we have at least one comment, load up the comment template.
