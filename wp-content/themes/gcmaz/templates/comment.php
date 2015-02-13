7892a043ff203f6300a2da36bb6e920abb014c8c
Stacy at home <stacy@stacymark.com>
2015-02-13 14:44:01 -0800

    update comment.php (individual comment) layout -give h color

--- wp-content/themes/gcmaz/templates/comment.php
+++ wp-content/themes/gcmaz/templates/comment.php
@@ -1,6 +1,6 @@
 <?php echo get_avatar($comment, $size = '64'); ?>
 <div class="media-body">
-  <h4 class="media-heading"><?php echo get_comment_author_link(); ?></h4>
+  <h4 class="media-heading black"><?php echo get_comment_author_link(); ?></h4>
   <time datetime="<?php echo comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a></time>
   <?php edit_comment_link(__('(Edit)', 'roots'), '', ''); ?>
 
