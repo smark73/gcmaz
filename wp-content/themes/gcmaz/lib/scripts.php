89731caf28ef78c493fc331ba02476a8984193a3
Stacy at home <stacy@stacymark.com>
2015-04-08 10:50:50 -0700

    tweak navbar css

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b05aebefd73fa0ae99edc5456f71bc2c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bcea55e60b1ea80130ea797354933d9d');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
--- wp-content/themes/gcmaz/templates/head.php
+++ wp-content/themes/gcmaz/templates/head.php
@@ -10,6 +10,7 @@
   
   <?php wp_head(); ?>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
   <?php if( is_front_page() ){ echo "<link type='text/css' rel='stylesheet' href='/assets/css/animate/animate.min.css'>"; }?>
     <!--[if lt IE 9]><script src="/assets/js/vendor/Respond/src/respond.js"></script><![endif]-->
 </head>
