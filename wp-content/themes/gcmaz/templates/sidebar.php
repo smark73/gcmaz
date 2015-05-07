05a1d6e5b340c59eae4c89a00b74deac2cc14a94
Stacy at home <stacy@stacymark.com>
2015-05-07 13:02:59 -0700

    fix wrong sidebar when search in sidebar.php

--- wp-content/themes/gcmaz/templates/sidebar.php
+++ wp-content/themes/gcmaz/templates/sidebar.php
@@ -4,23 +4,32 @@ $adv_id = get_page_by_title("Advertise On Northern Arizona Radio");
 //print_r($adv_id->ID);
 
 if(!empty( $post )){
-    //print_r($post->post_parent);
-
-    //homepage
-    if(is_front_page()){
+    
+    // need to keep is_search check first ... otherwise gets wrong sidebar as search info moves into post->info (a search for "concerts" calls the concerts sidebar)
+    if( is_search() ) {
+        //echo "<div style='background:yellow'>search</div>";
+        dynamic_sidebar('sidebar-primary');
+        
+    } elseif(is_front_page()){
+        //echo "<div style='background:yellow'>front page</div>";
         dynamic_sidebar('sidebar-homepage');
+        
     // list of pages to show basic (homepage) sidebar
     } elseif(is_page(array('Contact Great Circle Media', 'thank-you', 'employment'))){
         dynamic_sidebar('sidebar-homepage');
+        
     // advertising page and children
     } elseif(is_page('advertise-on-northern-arizona-radio') || $post->post_parent == $adv_id->ID){
         dynamic_sidebar('sidebar-homepage');
+        
     // weather
     } elseif($post->post_title == 'Area Weather'){
         dynamic_sidebar('sidebar-weather');
+        
     // splash pages
     }elseif(is_page_template('template-splash.php')){
         dynamic_sidebar('sidebar-splash');
+        
     // news etc get newsy sidebar
     } else {
         dynamic_sidebar('sidebar-primary');
