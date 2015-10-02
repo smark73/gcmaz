87cf98e91f7016612fb3570337e8635297c10c0a
Stacy at home <stacy@stacymark.com>
2015-10-02 14:27:38 -0700

    update styles to stop hiding top navbar when wpadminbar is visible

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c82bce6fa0db2dcf1918829a7355922');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '80da7cb05d49bda795bfd792e91b3c19');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
