ceb6e1d01e83b060f266924c4870cdb10124df90
Stacy at home <stacy@stacymark.com>
2015-10-07 16:25:04 -0700

    updated styles pertaining to PTO's and navbar

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '80da7cb05d49bda795bfd792e91b3c19');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2d8b4e62d064a3c0b694cc3dbe52486e');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
