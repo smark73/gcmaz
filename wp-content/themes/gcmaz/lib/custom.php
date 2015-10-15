8d1a1a12ca582ac7e0f854682018a911d0496b5e
Stacy at home <stacy@stacymark.com>
2015-10-15 08:44:52 -0700

    moved gf activate custom templates

--- wp-content/themes/gcmaz/lib/custom.php
+++ wp-content/themes/gcmaz/lib/custom.php
@@ -347,7 +347,7 @@ function gcmaz_user_msg(){
 add_action('wp', 'custom_maybe_activate_user', 9);
 function custom_maybe_activate_user() {
 
-    $template_path = STYLESHEETPATH . '/templates/gfur-activate-template/activate.php';
+    $template_path = STYLESHEETPATH . '/gfur-activate-template/activate.php';
     $is_activate_page = isset( $_GET['page'] ) && $_GET['page'] == 'gf_activation';
 
     if( ! file_exists( $template_path ) || ! $is_activate_page  )
