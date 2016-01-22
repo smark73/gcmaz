a64d29c75e0ec2310ba0c3d28ce8f1c3ab58b561
Stacy Mark <stacy@stacymark.com>
2016-01-22 11:20:07 -0800

    update to front page

--- wp-content/themes/gcmaz/front-page.php
+++ wp-content/themes/gcmaz/front-page.php
@@ -1,138 +1,145 @@
 <div class="row home-top">
     <div class="col-md-4 hidden-xs hidden-sm">
-         <?php  echo do_shortcode("[adrotate group='22']"); ?>
+        <?php  echo do_shortcode("[adrotate group='22']"); ?>
         <?php  echo the_widget( 'WP_Widget_Recent_Posts', 'Recent Posts from KAFF News', 5); ?>
     </div>
     <div class="col-md-8">
         <?php get_template_part('templates/content', 'page'); ?>
     </div>
-</div>
-<div class="row">
+    <div class="col-md-4 visible-xs visible-sm">
         <div class="row">
-            <div id="kaffBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="kaffLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kaff-logo.png" alt="92.9 KAFF Country" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://kaff.gcmaz.com" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://player.listenlive.co/36581" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-            
-            <div id="kmgnBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="kmgnLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kmgn-logo.png" alt="93-9 The Mountain" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://939themountain.gcmaz.com" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://player.listenlive.co/36601" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-            
-            <div id="kzglBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="kzglLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kzgl-logo.png" alt="103.7 The Eagle Rocks" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://eagle.gcmaz.com/" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://eagle.gcmaz.com/kzgl-stream-player/" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-
-            <div id="kfszBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="kfszLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kfsz-logo.png" alt="Hits 106" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://hits106.gcmaz.com" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://player.listenlive.co/36591" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-            
-            <div id="ktmgBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="ktmgLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ktmg-logo.png" alt="Magic 99.1" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://magic991.gcmaz.com" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://player.listenlive.co/36621" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-            
-            <div id="kaffamBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="kaffamLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kaffam-logo.png" alt="Flagstaff Country 93-5" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://country935.gcmaz.com" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://player.listenlive.co/36571" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-
-            <div id="e979Block" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="e979Logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/97-9-logo.png" alt="97.9 The Eagle Rocks" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://979theeagle.gcmaz.com/" target="_blank">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://979theeagle.gcmaz.com/stream-player/" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-            <div id="knotBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="knotLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/az-shine-logo.png" alt="Arizona Shine" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnDrk blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://arizonashine.org" target="_blank" style="color:#00416a;">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        <a href="http://stream.arizonashine.org:8000/arizonashine.mp3.m3u" target="_blank" style="color:#00416a;">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
-                    </div>
-                </div>
-            </div>
-            <div id="kaffnewsBlock" class="stationTile col-md-4 col-sm-6">
-                <div class="logoBlock">
-                    <img id="kaffnewsLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kaff-news-logo.png" alt="KAFF News of Northern Arizona" class="img-responsive"/>
-                </div>
-                <div class="row blockBtn blockBtnDrk blockBtnHide">
-                    <div class="col-xs-6 blockBtnWeb">
-                        <a href="http://gcmaz.com/kaff-news" target="_blank" style="color:#2d4a9f;">WEBSITE</a>
-                    </div>
-                    <div class="col-xs-6 blockBtnListen">
-                        &nbsp;
-                    </div>
-                </div>
+            <div class="col-md-6 col-sm-6 col-xs-6">
+                <?php  echo the_widget( 'WP_Widget_Recent_Posts', 'Recent Posts from KAFF News', 5); ?>
+             </div>
+             <div class="col-md-6 col-sm-6 col-xs-6" style="padding-top:25px;">
+                <?php  echo do_shortcode("[adrotate group='22']"); ?>
             </div>
         </div>
+    </div>
+</div>
+<div class="row">
+    <div id="kaffBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="kaffLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kaff-logo.png" alt="92.9 KAFF Country" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://kaff.gcmaz.com" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://player.listenlive.co/36581" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
     
+    <div id="kmgnBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="kmgnLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kmgn-logo.png" alt="93-9 The Mountain" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://939themountain.gcmaz.com" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://player.listenlive.co/36601" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+    
+    <div id="kzglBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="kzglLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kzgl-logo.png" alt="103.7 The Eagle Rocks" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://eagle.gcmaz.com/" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://eagle.gcmaz.com/kzgl-stream-player/" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+
+    <div id="kfszBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="kfszLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kfsz-logo.png" alt="Hits 106" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://hits106.gcmaz.com" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://player.listenlive.co/36591" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+    
+    <div id="ktmgBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="ktmgLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ktmg-logo.png" alt="Magic 99.1" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://magic991.gcmaz.com" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://player.listenlive.co/36621" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+    
+    <div id="kaffamBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="kaffamLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kaffam-logo.png" alt="Flagstaff Country 93-5" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://country935.gcmaz.com" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://player.listenlive.co/36571" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+
+    <div id="e979Block" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="e979Logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/97-9-logo.png" alt="97.9 The Eagle Rocks" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://979theeagle.gcmaz.com/" target="_blank">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://979theeagle.gcmaz.com/stream-player/" target="_blank">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+    <div id="knotBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="knotLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/az-shine-logo.png" alt="Arizona Shine" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnDrk blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://arizonashine.org" target="_blank" style="color:#00416a;">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                <a href="http://stream.arizonashine.org:8000/arizonashine.mp3.m3u" target="_blank" style="color:#00416a;">LISTEN<div class="spkrIcon"><i class="glyphicon glyphicon-volume-up"></i></div></a>
+            </div>
+        </div>
+    </div>
+    <div id="kaffnewsBlock" class="stationTile col-md-4 col-sm-6">
+        <div class="logoBlock">
+            <img id="kaffnewsLogo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/kaff-news-logo.png" alt="KAFF News of Northern Arizona" class="img-responsive"/>
+        </div>
+        <div class="row blockBtn blockBtnDrk blockBtnHide">
+            <div class="col-xs-6 blockBtnWeb">
+                <a href="http://gcmaz.com/kaff-news" target="_blank" style="color:#2d4a9f;">WEBSITE</a>
+            </div>
+            <div class="col-xs-6 blockBtnListen">
+                &nbsp;
+            </div>
+        </div>
+    </div>
 </div>
  
--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '52cecf047d1670eb04cc257c98eea8e6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1e35043ded2a51fe4188df4daf49d4ad');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
