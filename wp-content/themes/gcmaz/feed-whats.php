91f4f176a324e86a3220f78798672f4f05ad97db
Stacy Mark <stacy@stacymark.com>
2016-04-16 15:23:38 -0700

    updates to feed-whats.php - trying to fix

--- wp-content/themes/gcmaz/feed-whats.php
+++ wp-content/themes/gcmaz/feed-whats.php
@@ -122,7 +122,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
                                             <title><?php the_title_rss(); ?></title>
                                             <link><?php the_permalink_rss(); ?></link>
                                             <guid isPermaLink="false"><?php the_guid(); ?></guid>
-                                            <author><?php the_author(); ?></author>
+                                            <author><?php the_author_meta('user_email'); echo "("; the_author(); echo ")"; ?></author>
                                             <?php the_category_rss('rss2') ?>
                                             <?php if (get_option('rss_use_excerpt')) : ?>
                                                     <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
