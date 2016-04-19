5b4639b90c65c95bc5d6af02f5538b6ab3157245
Stacy Mark <stacy@stacymark.com>
2016-04-19 14:28:54 -0700

    fixing feed tpls

--- wp-content/themes/gcmaz/feed-whats.php
+++ wp-content/themes/gcmaz/feed-whats.php
@@ -40,108 +40,113 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 
 		<?php do_action( 'rss2_head' ); ?>
 		
-                            <?php
-                                //store the global post object temporarily and restore it after while loop
-                                $temp_post = $post;
-                                
-                                // get today
-                                $d = date('Ymd');
-                                
-                                // query the Whats Happening category
-                                // for position - use start date (whats_fulldate)
-                                // for kill date - use end date (whats_fullenddate)
-                                //  check if fulldate has a value of 20000101 (Jan 1, 2000) which is default given to non-dated items
-                                //  compare to todays date
-                                //  -- note 'posts_per_page' known to solve issue of some posts not showing up
-                                $args = array(
-                                    'post_type' => 'whats-happening',
-                                    'meta_key' => 'whats_fulldate',
-                                    'posts_per_page' => '999',
-                                    'orderby' => 'meta_value',
-                                    'order' => 'ASC',
-                                    'meta_query' => array(
-                                        array(
-                                            'key' => 'whats_fulldate',
-                                            'value' => array('20000101', $d),
-                                            'type' => 'date',
-                                            'compare' => '>=',
-                                        ),
-                                    ),
-                                  );
-                                
-                                $the_query = new WP_Query($args);
-                            ?>
-                            <!-- Start loop -->
-		<?php while( $the_query->have_posts()) : $the_query->the_post(); ?>
+            <?php
+                //store the global post object temporarily and restore it after while loop
+                $temp_post = $post;
+                
+                // get today
+                $d = date('Ymd');
+                
+                // query the Whats Happening category
+                // for position - use start date (whats_fulldate)
+                // for kill date - use end date (whats_fullenddate)
+                //  check if fulldate has a value of 20000101 (Jan 1, 2000) which is default given to non-dated items
+                //  compare to todays date
+                //  -- note 'posts_per_page' known to solve issue of some posts not showing up
+                $args = array(
+                    'post_type' => 'whats-happening',
+                    'meta_key' => 'whats_fulldate',
+                    'posts_per_page' => '999',
+                    'orderby' => 'meta_value',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'whats_fulldate',
+                            'value' => array('20000101', $d),
+                            'type' => 'date',
+                            'compare' => '>=',
+                        ),
+                    ),
+                  );
+                
+                $the_query = new WP_Query($args);
+            ?>
+
+            <!-- Start loop -->
+            <?php while( $the_query->have_posts()) : $the_query->the_post(); ?>
                             
-                                <?php
-                                    // check if in "Hide Post" category
-                                    if( !in_category( array( 'hide-post', 'Hide Post' ) ) ) :
-                                ?>
+                <?php
+                    // check if in "Hide Post" category
+                    if( !in_category( array( 'hide-post', 'Hide Post' ) ) ) :
+                ?>
                             
-                                    <?php
-                                        // double check date to see if its past date
-                                        $expDate = get_post_custom_values('whats_fullenddate');
-                                        if (!$expDate[0]){
-                                            // if no end date set, use start date or default value
-                                            $expDate = get_post_custom_values('whats_fulldate');
-                                        }
-                                        
-                                        if(($expDate[0] == '20000101') || (strtotime($expDate[0])) >= (strtotime('now'))) :
-                                    ?>
+                    <?php
+                        // double check date to see if its past date
+                        $expDate = get_post_custom_values('whats_fullenddate');
+                        if (!$expDate[0]){
+                            // if no end date set, use start date or default value
+                            $expDate = get_post_custom_values('whats_fulldate');
+                        }
+                        
+                        if(($expDate[0] == '20000101') || (strtotime($expDate[0])) >= (strtotime('now'))) :
+                    ?>
 
-                                        <?php
-                                            // get pertinent data and attach it to content variable
-                                            $content = get_the_content_feed('rss2');
+                        <?php
+                            // get pertinent data and attach it to content variable
+                            $content = get_the_content_feed('rss2');
 
-                                            $eStartDate = get_post_custom_values('whats_date');
-                                            $eEndDate = get_post_custom_values('whats_enddate');
-                                            
-                                            $eventStartDate = $eStartDate[0];
-                                            $eventEndDate = $eEndDate[0];
-                                            
-                                            if($eventEndDate){
-                                                //if we have an end date, add it
-                                                $eventDate = $eventStartDate . " - " . $eventEndDate;
-                                            } else {
-                                                $eventDate = $eventStartDate;
-                                            }
-                                            
-                                            $content = '<span class="listdate pull-right red">' . $eventDate . '</span>' . $content;
+                            $eStartDate = get_post_custom_values('whats_date');
+                            $eEndDate = get_post_custom_values('whats_enddate');
+                            
+                            $eventStartDate = $eStartDate[0];
+                            $eventEndDate = $eEndDate[0];
+                            
+                            if($eventEndDate){
+                                //if we have an end date, add it
+                                $eventDate = $eventStartDate . " - " . $eventEndDate;
+                            } else {
+                                $eventDate = $eventStartDate;
+                            }
+                            
+                            $content = '<span class="listdate pull-right red">' . $eventDate . '</span>' . $content;
 
-                                            if(has_post_thumbnail()) {
-                                                $postimages = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'thumbnail' );
-                                                $postimage = $postimages[0];
-                                                $imgTag = '<img src="' . $postimage . '" class="feed-img" align="left"/>';
-                                                $content = $imgTag . $content;
-                                            }
+                            if(has_post_thumbnail()) {
+                                $postimages = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'thumbnail' );
+                                $postimage = $postimages[0];
+                                $imgTag = '<img src="' . $postimage . '" class="feed-img" align="left"/>';
+                                $content = $imgTag . $content;
+                            }
 
-                                        ?>
+                        ?>
 
-                                        <item>
-                                            <title><?php the_title_rss(); ?></title>
-                                            <link><?php the_permalink_rss(); ?></link>
-                                            <guid isPermaLink="false"><?php the_guid(); ?></guid>
-                                            <author><?php the_author_meta('user_email'); echo "("; the_author(); echo ")"; ?></author>
-                                            <?php the_category_rss('rss2') ?>
-                                            <?php if (get_option('rss_use_excerpt')) : ?>
-                                                    <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
-                                            <?php else : ?>
-                                                    <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
-                                                    <?php if ( strlen( $content ) > 0 ) : ?>
-                                                            <content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
-                                                    <?php else : ?>
-                                                            <content:encoded><![CDATA[<?php the_excerpt_rss(); ?>]]></content:encoded>
-                                                    <?php endif; ?>
-                                            <?php endif; ?>
-                                        </item>
-
+                        <item>
+                            <title><?php the_title_rss(); ?></title>
+                            <link><?php the_permalink_rss(); ?></link>
+                            <guid isPermaLink="false"><?php the_guid(); ?></guid>
+                            <author><?php the_author_meta('user_email'); echo "("; the_author(); echo ")"; ?></author>
+                            <?php the_category_rss('rss2') ?>
+                            <?php if (get_option('rss_use_excerpt')) : ?>
+                                    <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
+                            <?php else : ?>
+                                    <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
+                                    <?php if ( strlen( $content ) > 0 ) : ?>
+                                            <content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
+                                    <?php else : ?>
+                                            <content:encoded><![CDATA[<?php the_excerpt_rss(); ?>]]></content:encoded>
                                     <?php endif; ?>
-                                <?php endif; ?>
-		<?php endwhile; ?>
-                            <?php
-                                // restore the global post object
-                                $post = $temp_post;
-                            ?>
+                            <?php endif; ?>
+                        </item>
+
+                    <?php endif; ?>
+
+                <?php endif; ?>
+
+            <?php endwhile; ?>
+
+            <?php
+                // restore the global post object
+                $post = $temp_post;
+            ?>
+            
 	</channel>
 </rss>
