50eee5b876cde3400d64f5b5ca242102e55bacf5
Stacy Mark <stacy@stacymark.com>
2016-05-19 15:02:02 -0700

    debugging sidebars

--- wp-content/themes/gcmaz/templates/sidebar.php
+++ wp-content/themes/gcmaz/templates/sidebar.php
@@ -1,4 +1,7 @@
 <?php
+
+$debug_page = true;
+
 global $post;
 
 // check if we're in News category
@@ -23,36 +26,62 @@ $adv_id = get_page_by_title("Advertise On Northern Arizona Radio");
 
 // need to keep is_search check first ... otherwise gets wrong sidebar as search info moves into post->info (a search for "concerts" calls the concerts sidebar)
 if( is_search() ) {
-    //echo "<div style='background:yellow'>search</div>";
+    if ($debug_page === true) {
+        echo "<div style='visibility:hidden;display:none;height:0;'>search</div>";
+    }
     dynamic_sidebar('sidebar-primary');
 
 } elseif(is_front_page()){
-    //echo "<div style='background:yellow'>front page</div>";
+    // no sidebar
+    //if ($debug_page === true) {
+        //echo "<div style='visibility:hidden;display:none;height:0;'>front page</div>";
+    //}
     dynamic_sidebar('sidebar-homepage');
     
 } elseif(!empty( $post )){
         
     // list of pages to show basic (homepage) sidebar
     if(is_page(array('Contact Great Circle Media', 'thank-you', 'employment'))){
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>contact, tx, employment</div>";
+        }
         dynamic_sidebar('sidebar-homepage');
         
     // advertising page and children
     } elseif(is_page('advertise-on-northern-arizona-radio') || $post->post_parent == $adv_id->ID){
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>adv</div>";
+        }
         dynamic_sidebar('sidebar-homepage');
         
     // weather
     } elseif($post->post_title == 'Area Weather'){
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>weather</div>";
+        }
         dynamic_sidebar('sidebar-weather');
         
     // splash pages
     } elseif(is_page_template('template-splash.php')){
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>splash</div>";
+        }
         dynamic_sidebar('sidebar-splash');
         
     // News
     } elseif( $post->post_name == 'kaff-news' ) {
+        // KAFF News home page
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>sidebar-news (home)</div>";
+        }
         // check if were on the kaff news main page
         dynamic_sidebar('sidebar-news');
+
     } elseif($show_news_sidebar == true) {
+        // KAFF News subpages
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>sidebar-news (subp)</div>";
+        }
         dynamic_sidebar('sidebar-news');
         // BELOW NOT USED - Changed to 1 sidebar instead of separate sidebars (see widgets.php)
         // changed - so can have banner for Pres AND Flag on post with both cat's
@@ -70,11 +99,16 @@ if( is_search() ) {
         
     // etc 
     } else {
+        if ($debug_page === true) {
+            echo "<div style='visibility:hidden;display:none;height:0;'>primary-sidebar(1)</div>";
+        }
         dynamic_sidebar('sidebar-primary');
     }
 
 } else {
-    
+    if ($debug_page === true) {
+        echo "<div style='visibility:hidden;display:none;height:0;'>primary-sidebar(2)</div>";
+    }
     dynamic_sidebar('sidebar-primary');
     
 }
