6923c82f10aa2155230bd2e18c45c421c1257db2
Stacy Mark <stacy@stacymark.com>
2016-05-24 10:47:09 -0700

    update weather styles

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '78c6b0d88d0e7475b070626a35f00c5f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c565641f7be6535db988dd50a124768');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
