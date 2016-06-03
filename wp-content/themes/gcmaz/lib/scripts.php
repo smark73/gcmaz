5ac0a1f69684833ed9947dfb28239cccacd82d54
Stacy Mark <stacy@stacymark.com>
2016-06-03 11:04:05 -0700

    update single event layout and styles

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'de82cec0a24c5132964c4d24a3590bfb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd9deb834caa9d42618523f8a59b25dac');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/gcmaz/single-gcmaz-event.php
+++ wp-content/themes/gcmaz/single-gcmaz-event.php
@@ -6,26 +6,6 @@
     </div>
     <?php while (have_posts()) : the_post(); ?>
       <article <?php post_class('sngl-info'); ?>>
-          <div class="pull-right hidden-xs hidden-sm">
-                <?php
-                    if(has_post_thumbnail()){
-                        $full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
-                        echo '<a href="' . $full_image_url[0] . '" target="_blank" title="' . the_title_attribute( 'echo=0' ) . '">';
-                        the_post_thumbnail('medium', array('class'=>'info-img'));
-                        echo '</a>';
-                    }
-                ?>
-          </div>
-          <div class="centered visible-xs visible-sm">
-                <?php
-                    if(has_post_thumbnail()){
-                        $full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
-                        echo '<a href="' . $full_image_url[0] . '" target="_blank" title="' . the_title_attribute( 'echo=0' ) . '">';
-                        the_post_thumbnail('medium', array('class'=>'info-img'));
-                        echo '</a>';
-                    }
-                ?>
-          </div>
         <header>
             <h3 class='blue'><?php the_title(); ?></h3>
             <span class='red' style='font-size:1.3em'>
@@ -40,7 +20,19 @@
             </span>
         </header>
         <div class="entry-content">
-          <?php the_content(); ?>
+
+            <?php
+                // image
+                if(has_post_thumbnail()){
+                    $full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+                    echo '<a href="' . $full_image_url[0] . '" target="_blank" title="' . the_title_attribute( 'echo=0' ) . '" class="event-img-link">';
+                        the_post_thumbnail('medium', array('class'=>'info-img'));
+                    echo '</a>';
+                }
+            ?>
+
+            <?php the_content(); ?>
+
         </div>
         <footer>
           <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
