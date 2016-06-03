ba969de82feac12fb85278c8e71215420cdcc26c
Stacy Mark <stacy@stacymark.com>
2016-06-02 18:23:40 -0700

    change styles in tpl events-by-cat

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c565641f7be6535db988dd50a124768');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '21934526bcf0e917da35a274cde27d6b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/gcmaz/template-events-by-cat.php
+++ wp-content/themes/gcmaz/template-events-by-cat.php
@@ -70,15 +70,17 @@ if ( stripos( $cpt_slug, 'community' ) !== false ){
             
             if( ( $expDate[0] == '20000101' ) || ( strtotime($expDate[0]) ) >= ( strtotime('now') ) ) : ?>
                 <section class="archv-pg-lstng row">
-                    <?php if(has_post_thumbnail()) : ?>
-                        <div class="archv-thmb col-md-3 col-sm-4 hidden-xs">
-                            <?php the_post_thumbnail('thumbnail');?>
-                        </div>
-                        <div class="centered visible-xs">
-                            <?php the_post_thumbnail('thumbnail');?>
-                       </div>
-                       <div class="archv-info col-md-9 col-sm-8 col-xs-12">
-                            <span class="archv-date pull-right red">
+
+                       <div class="archv-info col-md-12 col-sm-12 col-xs-12">
+
+                           <?php
+                                //img visible on sm screen
+                                if(has_post_thumbnail()) {
+                                    the_post_thumbnail('medium', array( 'class' => 'aligncenter hidden-md hidden-lg' ) ) ;
+                                }
+                            ?>
+
+                            <p class="archv-date">
                                 <?php
                                     // echo date
                                     $startdate = get_post_custom_values('event_start_date');
@@ -89,26 +91,19 @@ if ( stripos( $cpt_slug, 'community' ) !== false ){
                                         echo " - " . $enddate[0];
                                     }
                                  ?>
-                            </span>
+                            </p>
+
+                            <?php
+                                //img hidden on sm screen
+                                if(has_post_thumbnail()) {
+                                    the_post_thumbnail('thumbnail', array( 'class' => 'alignright hidden-sm hidden-xs' ) ) ;
+                                }
+                            ?>
+
                             <?php get_template_part('templates/content', get_post_format());?>
+
                        </div>
-                    <?php else : ?>
-                        <div class="archv-info col-md-12 col-sm-12 col-xs-12">
-                            <span class="archv-date pull-right red">
-                                <?php
-                                    // echo date
-                                    $startdate = get_post_custom_values('event_start_date');
-                                    echo $startdate[0];
-                                    // if has end date, add it
-                                    $enddate = get_post_custom_values('event_end_date');
-                                    if($enddate[0]){
-                                        echo " - " . $enddate[0];
-                                    }
-                                ?>
-                            </span>
-                            <?php get_template_part('templates/content', get_post_format());?>
-                        </div>
-                    <?php endif;?>
+
                     <div class="clearfix"></div>
                     <hr class="archv-pg-hr">
                 </section>
