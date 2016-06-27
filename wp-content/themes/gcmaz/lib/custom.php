4c49ae6d631d6a2251fb0086a40ae104ef40018d
Stacy Mark <stacy@stacymark.com>
2016-06-27 14:12:33 -0700

    renamed posts label to KAFF News

--- wp-content/themes/gcmaz/lib/custom.php
+++ wp-content/themes/gcmaz/lib/custom.php
@@ -410,6 +410,40 @@ function gcmaz_dashboard_widgets(){
 // END
 
 
+// Rename "Posts" to "KAFF News"
+function change_news_post_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'KAFF News';
+    $submenu['edit.php'][5][0] = 'See All News';
+    $submenu['edit.php'][10][0] = 'Add News Post';
+    $submenu['edit.php'][16][0] = 'News Tags';
+    echo '';
+}
+function change_news_post_object() {
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'KAFF News';
+    $labels->singular_name = 'News Post';
+    $labels->add_new = 'Add News Post';
+    $labels->add_new_item = 'Add News Post';
+    $labels->edit_item = 'Edit News';
+    $labels->new_item = 'News';
+    $labels->view_item = 'View News';
+    $labels->search_items = 'Search News';
+    $labels->not_found = 'No News found';
+    $labels->not_found_in_trash = 'No News found in Trash';
+    $labels->all_items = 'All News';
+    $labels->menu_name = 'News';
+    $labels->name_admin_bar = 'News';
+}
+add_action( 'admin_menu', 'change_news_post_label' );
+add_action( 'init', 'change_news_post_object' );
+
+
+
+
+
 /*
  * SimplePie function to shorten feed 
  */
