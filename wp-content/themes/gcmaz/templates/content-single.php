0eafd7aad988d47dd1d6987e352d1c06da681895
Stacy Mark <stacy@stacymark.com>
2016-09-01 09:05:05 -0700

    update kaff news categories display - remove slider

--- wp-content/themes/gcmaz/templates/content-single.php
+++ wp-content/themes/gcmaz/templates/content-single.php
@@ -1,7 +1,19 @@
 <div class="in-cnt-wrp row">
     <?php while (have_posts()) : the_post(); ?>
         <div class="centered rbn-hdg">
-            <span class="txtshdw hdr-cat-links"><?php the_category('  |  '); ?></span>
+            <span class="txtshdw hdr-cat-links">
+                <?php
+                    //the_category('  |  ');
+                    // display News categories except "slider"
+                    $categories = get_the_category();
+                    foreach( $categories as $cat ){
+                        //print_r($cat);
+                        if( $cat->slug !== 'kaff-news-slider' ){
+                            echo '<a href="' . get_category_link( $cat->term_id ) . '" title="' . sprintf( __( "%s" ), $cat->name ) . '" ' . '>' . $cat->name.'</a> ';
+                        }
+                    }
+                ?>
+            </span>
         </div>
         <article class="entry-content">
             <h3 class="story-hdr"><?php the_title(); ?></h3>
