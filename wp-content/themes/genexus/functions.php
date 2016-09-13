0a7ed6d47ea63ad0f0cf349d84516e497a42ed9b
Stacy Mark <stacy@stacymark.com>
2016-09-13 07:58:21 -0700

    make fn param optional in pagination fn

--- wp-content/themes/genexus/functions.php
+++ wp-content/themes/genexus/functions.php
@@ -863,10 +863,13 @@ add_filter('user_contactmethods', 'genexus_user_contactmethods', 10, 1);
  * @global $wp_query http://codex.wordpress.org/Class_Reference/WP_Query
  * @return Prints the HTML for the pagination if a template is $paged
  */
-function base_pagination( $the_query ) {
+function base_pagination( $the_query = NULL ) {
     global $wp_query;
 
-    $wp_query = $the_query;
+    if (!empty( $the_query ) ){
+        // overwrite wp_query with the passed one if exists
+        $wp_query = $the_query;
+    }
 
     $big = 999999999; // This needs to be an unlikely integer
 
